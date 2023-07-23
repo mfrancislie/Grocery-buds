e57e1e994b0634a7b113c098923c4834eacb0eab
Francislie <mfrancislie@gmail.com>
2023-07-23 01:43:16 -0700

    add item

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './Form';
+import { nanoid } from 'nanoid';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      id: nanoid(),
+      name: itemName,
+      completed: false,
+    };
+
+    setItems([...items, newItem]);
+    console.log('item added to the list');
+  };
   return (
     <section className="section-center">
-      <Form />
+      <Form addItem={addItem} />
     </section>
   );
 }
